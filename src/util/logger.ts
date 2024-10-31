2cb9766a1c8d7c82dcde928fa64cf09d46d243c0
Aniket Pradhan <aniket@shootorder.in>
2024-10-31 20:30:56 +0530

    feat: Colorized termianl

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -4,28 +4,44 @@ import util from 'util'
 import config from '../config/config'
 import { EApplicationEnvironment } from '../constant/application'
 import path from 'path'
+import {red, blue, yellow, green, magenta} from 'colorette'
 import * as sourceMapSupport from 'source-map-support';
 
 // Linking trace support
 sourceMapSupport.install()
 
+const colorizeLevel = (level: string) => {
+    switch (level) {
+        case 'ERROR':
+            return red(level)
+        case 'INFO':
+            return blue(level)
+        case 'WARN':
+            return yellow(level)
+        default:
+            return level
+    }
+}
+
 const customLogFormat = format.printf((info) => {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const { level, message, timestamp, meta = {} } = info
 
-    const customLevel = level.toUpperCase()
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const customTimeStamp = timestamp
+    const customLevel = colorizeLevel(level.toUpperCase())
+
+     
+    const customTimeStamp = green(timestamp as string)
 
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const customMessage = message
 
     const customMeta = util.inspect(meta, {
         showHidden: false,
-        depth: null
+        depth: null,
+        colors: true
     })
 
-    const customLog = `${customLevel} [${customTimeStamp}] ${customMessage} \n${'META'} ${customMeta}\n`
+    const customLog = `${customLevel} [${customTimeStamp}] ${customMessage} \n${magenta('META')} ${customMeta}\n`
 
     return customLog
 })
